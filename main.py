4e817592bad24e1e57620946b99a66d1e6c66289
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2024-05-01 20:47:20 -0500

    Added more tables

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def setup_tables():
                   Username varchar(80) NOT NULL,\
                   name varchar(80) NOT NULL,\
                   password varchar(80) NOT NULL,\
-                  balance int NOT NULL,\
+                  balance float NOT NULL,\
                   contacts JSON NOT NULL,\
                   notifications JSON NOT NULL,\
                   listed bool NOT NULL,\
@@ -31,8 +31,46 @@ def setup_tables():
                   weekly_hours_worked float,\
                   hourly_pay float,\
                   Business_name varchar(80),\
+                  benefits JSON,\
                   PRIMARY KEY(Username)\
                   );") # Needs something like CONSTRAINT `brand_id` FOREIGN KEY(`brand_id`) REFERENCES `e_store`.`brands`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE , once other tables are added
+
+  cursor.execute("DROP TABLE IF EXISTS notification")
+  cursor.execute("CREATE TABLE IF NOT EXISTS notification(\
+                  Notification_id varchar(80) NOT NULL,\
+                  source varchar(16) NOT NULL,\
+                  Username varchar(80),\
+                  Business varchar(80),\
+                  type int NOT NULL,\
+                  subject varchar(80) NOT NULL,\
+                  message varchar(400) NOT NULL,\
+                  Refund_id varchar(80),\
+                  PRIMARY KEY(Notification_id)\
+                  );")
+
+  cursor.execute("DROP TABLE IF EXISTS transaction")
+  cursor.execute("CREATE TABLE IF NOT EXISTS transaction(\
+                  Transaction_id varchar(80) NOT NULL,\
+                  money_received float NOT NULL,\
+                  money_sent float NOT NULL,\
+                  source varchar(16) NOT NULL,\
+                  Username varchar(80),\
+                  Business varchar(80),\
+                  timestamp int NOT NULL,\
+                  Refund_id varchar(80) NOT NULL,\
+                  PRIMARY KEY(Transaction_id)\
+                  );")
+
+  cursor.execute("DROP TABLE IF EXISTS business")
+  cursor.execute("CREATE TABLE IF NOT EXISTS business(\
+                  business_name varchar(80) NOT NULL,\
+                  products JSON NOT NULL,\
+                  balance float NOT NULL,\
+                  refunds JSON NOT NULL,\
+                  employees JSON NOT NULL,\
+                  transactions JSON NOT NULL,\
+                  PRIMARY KEY(business_name)\
+                  );")
 
 
 mydb = create_server_connection("localhost", "root", "auto", "mydatabase")
@@ -43,6 +81,24 @@ print(mydb.get_server_version())
 setup_tables()
 
 cursor.execute("DESC user_account")
+
+for x in cursor:
+  print(x)
+
+print("\n")
+cursor.execute("DESC notification")
+
+for x in cursor:
+  print(x)
+
+print("\n")
+cursor.execute("DESC transaction")
+
+for x in cursor:
+  print(x)
+
+print("\n")
+cursor.execute("DESC business")
 
 for x in cursor:
   print(x)
